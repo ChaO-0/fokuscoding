3929d8f62a9ce23427a7e53a1b324fffd515f2d0
Christopher Yu <christophergin88@gmail.com>
2021-01-23 02:54:56 +0800

    updated common module

--- common/src/middlewares/current-user.ts
+++ common/src/middlewares/current-user.ts
@@ -2,34 +2,35 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 interface UserPayload {
-  id: string;
-  email: string;
-  username: string;
+	id: string;
+	email: string;
+	username: string;
+	admin: boolean;
 }
 
 declare global {
-  namespace Express {
-    interface Request {
-      currentUser?: UserPayload;
-    }
-  }
+	namespace Express {
+		interface Request {
+			currentUser?: UserPayload;
+		}
+	}
 }
 
 export const currentUser = (
-  req: Request,
-  res: Response,
-  next: NextFunction
+	req: Request,
+	res: Response,
+	next: NextFunction
 ) => {
-  if (!req.session?.jwt) {
-    return next();
-  }
+	if (!req.session?.jwt) {
+		return next();
+	}
 
-  try {
-    const payload = jwt.verify(
-      req.session.jwt,
-      process.env.JWT_KEY!
-    ) as UserPayload;
-    req.currentUser = payload;
-  } catch (err) {}
-  next();
+	try {
+		const payload = jwt.verify(
+			req.session.jwt,
+			process.env.JWT_KEY!
+		) as UserPayload;
+		req.currentUser = payload;
+	} catch (err) {}
+	next();
 };
