0712918a292776b14b3844a00ec48bcfefad790f
Fajar Alnito <fajaralnito18@gmail.com>
2021-02-13 12:48:58 +0800

    min query to 3

--- search/src/routes/search.ts
+++ search/src/routes/search.ts
@@ -1,23 +1,29 @@
+import { validateRequest } from '@heapoverflow/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Post } from '../models/Post';
 
 const router = express.Router();
 
-router.post('/api/search', async (req: Request, res: Response) => {
-	const { query } = req.body;
-	const search = await Post.find({
-		title: {
-			$regex: `.*${query}.*`,
-			$options: 'i',
-		},
-	});
+router.post(
+	'/api/search',
+	[
+		body('query')
+			.isLength({ min: 3 })
+			.withMessage('keyword must be 3 characters'),
+	],
+	validateRequest,
+	async (req: Request, res: Response) => {
+		const { query } = req.body;
+		const search = await Post.find({
+			title: {
+				$regex: `.*${query}.*`,
+				$options: 'i',
+			},
+		});
 
-	console.log(search);
-
-	console.log(query);
-
-	return res.send(search);
-});
+		return res.send(search);
+	}
+);
 
 export { router as SearchRouter };
