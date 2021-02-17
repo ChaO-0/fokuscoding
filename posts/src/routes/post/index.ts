31c58275b66d62b887622cf91950cfef3ce80fed
Christopher Yu <christophergin88@gmail.com>
2021-02-17 11:51:03 +0800

    updated index post route

--- posts/src/routes/post/index.ts
+++ posts/src/routes/post/index.ts
@@ -6,8 +6,22 @@ const router = express.Router();
 router.get('/api/posts', async (req: Request, res: Response) => {
 	// fetch all posts
 	const posts = await Post.find().populate('tags').populate('votes');
+	const { limit, offset } = req.query;
 
-	return res.send(posts);
+	const count = await Post.estimatedDocumentCount();
+	const entries = await Post.find()
+		.populate('tags')
+		.populate('votes')
+		.limit(limit ? parseInt(limit as string) : 0)
+		.skip(offset ? parseInt(offset as string) : 0);
+	console.log(count);
+
+	return res.send({
+		entries,
+		limit: req.query.limit,
+		offset: req.query.offset,
+		total_count: count,
+	});
 });
 
 export { router as indexPostRouter };
