81554987f09169d0214723214c4d741513c9bdb6
Fajar Alnito <fajaralnito18@gmail.com>
2021-02-21 16:15:54 +0800

    updates

--- posts/src/routes/post/show.ts
+++ posts/src/routes/post/show.ts
@@ -1,6 +1,6 @@
 import { NotFoundError } from '@heapoverflow/common';
 import express, { Request, Response } from 'express';
-import { Post } from '../../models/Post';
+import { Post, PostDoc } from '../../models/Post';
 
 const router = express.Router();
 
@@ -9,7 +9,8 @@ router.get('/api/posts/:post_id', async (req: Request, res: Response) => {
 	const post = await Post.findById(req.params.post_id)
 		.populate('votes')
 		.populate('comments')
-		.populate('tags');
+		.populate('tags')
+		.populate('solution');
 
 	// check if the post is exist
 	if (!post) {
