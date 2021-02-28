44aa6360dd3c9ef2ae84cdaa188e1ba89039263e
Christopher Yu <christophergin88@gmail.com>
2021-02-28 20:32:02 +0800

    added style for testing home route

--- client/pages/home.js
+++ client/pages/home.js
@@ -1,5 +1,39 @@
-const Home = () => {
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+
+const Home = ({ currentUser }) => {
+	const router = useRouter();
+	const user = useSelector((state) => state.authReducer.user);
+	useEffect(() => {
+		if (!currentUser) router.push('/');
+		console.log(user);
+	}, []);
 	return <h1>Test</h1>;
+};
+
+export const getServerSideProps = async ({ req, res }) => {
+	const { data } = await axios.get(
+		`${process.env.INGRESS_URI}/api/users/currentuser`,
+		{
+			headers: req.headers,
+		}
+	);
+	console.log(data);
+	if (!data.currentUser) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		};
+	}
+	return {
+		props: {
+			currentUser: data.currentUser,
+		},
+	};
 };
 
 export default Home;
