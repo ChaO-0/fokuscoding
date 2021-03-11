7552d1718e39f031dbdddb4f97604db93f5179f4
Fajar Alnito <fajaralnito18@gmail.com>
2021-03-11 20:24:07 +0800

    add toast component

---
+++ client/components/Toast.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { Snackbar } from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
+import { useDispatch, useSelector } from 'react-redux';
+import { open } from '../redux/ducks/openload';
+
+const Toast = (props) => {
+	const dispatch = useDispatch();
+
+	const openValue = useSelector((state) => state.openLoad.open);
+
+	const handleClose = (event, reason) => {
+		if (reason === 'clickaway') {
+			return;
+		}
+
+		dispatch(open(false));
+	};
+	return (
+		<Snackbar
+			anchorOrigin={{
+				vertical: 'top',
+				horizontal: 'center',
+			}}
+			open={openValue}
+			autoHideDuration={2000}
+			onClose={handleClose}
+		>
+			<Alert elevation={6} variant="filled" {...props} onClose={handleClose}>
+				{props.children}
+			</Alert>
+		</Snackbar>
+	);
+};
+
+export default Toast;
