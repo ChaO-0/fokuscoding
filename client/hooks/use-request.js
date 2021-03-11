2e8457061c393a7392f5a60bfefbdf43ed1b90d4
Fajar Alnito <fajaralnito18@gmail.com>
2021-03-11 20:24:24 +0800

    add some logic

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -11,7 +11,9 @@ const useRequest = ({ url, method, onSuccess }) => {
 
 			if (onSuccess) {
 				onSuccess(response.data);
-				localStorage.setItem('currentUser', JSON.stringify(response.data));
+				if (response.data.currentUser) {
+					localStorage.setItem('currentUser', JSON.stringify(response.data));
+				}
 			}
 
 			return response.data;
