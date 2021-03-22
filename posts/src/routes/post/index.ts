bba59179e91b32b4856a2e624c9a908476f3f2a2
Fajar Alnito <fajaralnito18@gmail.com>
2021-03-22 21:09:14 +0800

    add sort

--- posts/src/routes/post/index.ts
+++ posts/src/routes/post/index.ts
@@ -6,13 +6,15 @@ const router = express.Router();
 router.get('/api/posts', async (req: Request, res: Response) => {
 	const limit: number = parseInt(req.query.limit as string);
 	const offset: number = parseInt(req.query.offset as string);
-
 	const posts = await Post.paginate(
 		{},
 		{
 			populate: ['votes', 'comments', 'tags'],
 			offset: offset || 0,
 			limit: limit || 10,
+			sort: {
+				updatedAt: 'desc',
+			},
 		}
 	);
 
