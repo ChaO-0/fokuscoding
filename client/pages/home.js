32a10ae00fa4c1beae36b5ce14039f3259c48d76
Christopher Yu <christophergin88@gmail.com>
2021-03-24 01:35:12 +0800

    added infinite scroll

--- client/pages/home.js
+++ client/pages/home.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import PostList from '../components/PostList';
 import Layout from '../components/Layout';
 
 const Home = ({ currentUser, posts }) => {
+	const [nextPosts, setNextPosts] = useState(posts.docs);
+	const [offset, setOffset] = useState(5);
+	const [hasMore, setHasMore] = useState(true);
+
+	const fetchMoreData = async () => {
+		const { data } = await axios.get(`/api/posts?offset=${offset}&limit=5`);
+		if (data.docs.length === 0) {
+			setHasMore(false);
+		}
+
+		setTimeout(() => {
+			setNextPosts((prev) => [...prev, ...data.docs]);
+		}, 1500);
+		setOffset((prev) => prev + 5);
+		console.log(nextPosts);
+	};
+
 	return (
 		<Layout currentUser={currentUser}>
 			<>
-				{posts.docs.map((post) => (
-					<PostList
-						key={post.id}
-						title={post.title}
-						voteCount={
-							post.votes.filter((vote) => vote.type === 'up').length -
-							post.votes.filter((vote) => vote.type === 'down').length
-						}
-						tags={post.tags}
-						createdBy={post.username}
-						time={moment(post.updatedAt).fromNow()}
-					/>
-				))}
+				<InfiniteScroll
+					dataLength={nextPosts.length}
+					next={fetchMoreData}
+					hasMore={hasMore}
+					loader={<h4>loading</h4>}
+					endMessage={<h1>This is end</h1>}
+				>
+					{nextPosts.map((post) => (
+						<PostList
+							key={post.id}
+							title={post.title}
+							voteCount={
+								post.votes.filter((vote) => vote.type === 'up').length -
+								post.votes.filter((vote) => vote.type === 'down').length
+							}
+							tags={post.tags}
+							createdBy={post.username}
+							time={moment(post.updatedAt).fromNow()}
+						/>
+					))}
+				</InfiniteScroll>
 			</>
 		</Layout>
 	);
@@ -35,7 +61,7 @@ export const getServerSideProps = async ({ req }) => {
 	);
 
 	const { data: posts } = await axios.get(
-		`${process.env.INGRESS_URI}/api/posts`,
+		`${process.env.INGRESS_URI}/api/posts?offset=0&limit=5`,
 		{
 			headers: req.headers,
 		}
