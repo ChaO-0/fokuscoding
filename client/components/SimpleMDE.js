eebed6301465d7d09aefd873d016f091b916a69c
Christopher Yu <christophergin88@gmail.com>
2021-03-25 22:02:38 +0800

    added component for dynamic import

---
+++ client/components/SimpleMDE.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import SimpleMDE from 'react-simplemde-editor';
+import 'easymde/dist/easymde.min.css';
+
+const DynamicSimpleMDE = () => {
+	const [content, setContent] = useState('');
+
+	return (
+		<SimpleMDE
+			value={content}
+			onChange={(value) => setContent(value)}
+			options={{
+				hideIcons: ['guide', 'fullscreen', 'side-by-side'],
+				showIcons: ['code', 'table'],
+			}}
+		/>
+	);
+};
+
+export default DynamicSimpleMDE;
