0e2239ffd53aba52d38960ea24c81517d0614e08
Christopher Yu <christophergin88@gmail.com>
2021-03-26 21:15:39 +0800

    update createpost

--- client/pages/post/createpost.js
+++ client/pages/post/createpost.js
@@ -1,12 +1,33 @@
 import Layout from '../../components/Layout';
 import PostForm from '../../components/PostForm';
+import axios from 'axios';
 
-const CreatePost = () => {
+const CreatePost = ({ tags }) => {
+	console.log(tags);
 	return (
 		<Layout currentUser={{ username: 'pram' }}>
-			<PostForm />
+			<PostForm tags={tags} />
 		</Layout>
 	);
+};
+
+export const getServerSideProps = async ({ req }) => {
+	const { data } = await axios.get(`${process.env.INGRESS_URI}/api/tags`, {
+		headers: req.headers,
+	});
+
+	data.forEach((elm) => {
+		delete elm.description;
+		delete elm.status;
+		delete elm.version;
+		delete elm.username;
+	});
+
+	return {
+		props: {
+			tags: data,
+		},
+	};
 };
 
 export default CreatePost;
