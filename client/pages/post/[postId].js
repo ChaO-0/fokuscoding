7dae45e85085d6cad48295f21c724d61322e94f7
Christopher Yu <christophergin88@gmail.com>
2021-03-31 03:22:55 +0800

    updated a bit of detailed post

--- client/pages/post/[postId].js
+++ client/pages/post/[postId].js
@@ -2,11 +2,53 @@ import axios from 'axios';
 import Layout from '../../components/Layout';
 import renderToString from 'next-mdx-remote/render-to-string';
 import hydrate from 'next-mdx-remote/hydrate';
+import { Card, CardContent, Box, Typography, Chip } from '@material-ui/core';
 
 const PostShow = ({ post, mdxContent }) => {
 	console.log(post);
 	const content = hydrate(mdxContent);
-	return <Layout currentUser={{ username: 'admin' }}>{content}</Layout>;
+	return (
+		<Layout currentUser={{ username: 'admin' }}>
+			<Card>
+				<CardContent>
+					<Box display="flex" flexDirection="row" justifyContent="flex-start">
+						<Box flexDirection="column" marginRight={3}>
+							<Typography variant="h5" color="secondary" align="center">
+								<Box fontWeight={600}>{0}</Box>
+							</Typography>
+							<Typography
+								color="secondary"
+								component="div"
+								style={{ fontSize: 12 }}
+								align="center"
+							>
+								<Box fontWeight={600} my="auto">
+									VOTE
+								</Box>
+							</Typography>
+						</Box>
+						<Box flexDirection="row">
+							<Box>
+								{post.tags.map((tag) => (
+									<Chip
+										label={tag.name}
+										key={tag.id}
+										style={{
+											backgroundColor: '#4CC9B040',
+											color: '#4CC9B0',
+											borderRadius: 0,
+											margin: '0 3',
+										}}
+									/>
+								))}
+							</Box>
+							<Typography variant="h4">{post.title}</Typography>
+						</Box>
+					</Box>
+				</CardContent>
+			</Card>
+		</Layout>
+	);
 };
 
 export default PostShow;
