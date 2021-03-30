4b83accf5ac7a71906a8ef11b7ddc3bd006b2895
Christopher Yu <christophergin88@gmail.com>
2021-03-31 02:55:24 +0800

    now user can submit a post

--- client/components/PostForm.js
+++ client/components/PostForm.js
@@ -100,8 +100,7 @@ const PostForm = ({ tags: tagsList }) => {
 				validationSchema={validationSchema}
 				initialValues={{ title: '', tags: [], body: '' }}
 				onSubmit={(values) => {
-					console.log(values);
-					// doRequest(values);
+					doRequest(values);
 				}}
 			>
 				{({ setFieldValue, setFieldTouched }) => (
