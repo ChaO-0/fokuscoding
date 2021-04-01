b08e5fb8511d6ec9374a63efe8a4bf00ffbfdaa9
Fajar Alnito <fajaralnito18@gmail.com>
2021-04-01 21:56:00 +0800

    admin / remove admin

--- auth/src/routes/upgrade.ts
+++ auth/src/routes/upgrade.ts
@@ -1,21 +1,34 @@
 import { NotAuthorizedError, requireAuth } from '@heapoverflow/common';
 import express from 'express';
-import { User } from '../models/User';
+import { User, UserDoc } from '../models/User';
 
 const router = express.Router();
 
 router.post('/api/users/:user_id/upgrade', requireAuth, async (req, res) => {
-	const user = await User.findById(req.params.user_id);
+	const user: UserDoc = await User.findById(req.params.user_id);
 
 	if (!req.currentUser!.admin) {
 		throw new NotAuthorizedError();
 	}
 
-	user.set({
-		is_admin: true,
-	});
+	if (
+		req.currentUser?.admin &&
+		req.currentUser.username === 'admin' &&
+		user.is_admin
+	) {
+		console.log('here admin');
 
-	await user.save();
+		user.set({
+			is_admin: false,
+		});
+		await user.save();
+	} else {
+		user.set({
+			is_admin: true,
+		});
+
+		await user.save();
+	}
 
 	res.send(user);
 });
