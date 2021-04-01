9182b06405438ee960bc742dc870ede54226ecc2
Christopher Yu <christophergin88@gmail.com>
2021-04-01 23:02:23 +0800

    now the user can comment

--- client/pages/post/[postId].js
+++ client/pages/post/[postId].js
@@ -6,26 +6,59 @@ import { Card, CardContent, Box, Typography, Button } from '@material-ui/core';
 import dynamic from 'next/dynamic';
 import PostDetail from '../../components/PostDetail';
 import { Form, Formik } from 'formik';
+import useRequest from '../../hooks/use-request';
+import { useRouter } from 'next/router';
+import {
+	ExpandLess as ExpandLessIcon,
+	ExpandMore as ExpandMoreIcon,
+} from '@material-ui/icons';
+import CommentList from '../../components/CommentList';
+import { useState } from 'react';
 
 const SimpleMDE = dynamic(() => import('../../components/SimpleMDE'), {
 	ssr: false,
 });
 
-const PostShow = ({ post, mdxContent }) => {
-	// console.log(post);
+const PostShow = ({ post, mdxContent, comments }) => {
+	// const [postComments, setPostComments] = useState([...comments]);
+
+	// console.log(postComments);
+
+	const router = useRouter();
+	const { postId } = router.query;
+	const { doRequest } = useRequest({
+		url: `/api/posts/${postId}`,
+		method: 'post',
+		onSuccess: () => router.push(`/post/${postId}`),
+	});
+
 	const content = hydrate(mdxContent);
+	const test = [];
+
+	comments.map((elm) => {
+		test.push(hydrate(elm));
+	});
+
+	// const test_ = post.comments.map((comment, idx) => {
+	// 	return { ...comment, mdxed: test[idx] };
+	// });
+
 	return (
 		<Layout currentUser={{ username: 'admin' }}>
 			<>
 				<PostDetail post={post} content={content} />
 				<Box mt={3}>
 					<Typography variant="h4" color="secondary">
-						0 Jawaban
+						{post.comments.length} Jawaban
 					</Typography>
 					<hr />
-					<Card>
-						<CardContent>test</CardContent>
-					</Card>
+					{post.comments.map((comment, idx) => {
+						return (
+							<CommentList key={comment.id} comment={comment}>
+								{test[idx]}
+							</CommentList>
+						);
+					})}
 				</Box>
 				<Box mt={3}>
 					<Typography variant="h4" color="secondary">
@@ -33,17 +66,17 @@ const PostShow = ({ post, mdxContent }) => {
 					</Typography>
 					<hr />
 					<Formik
-						initialValues={{ body: '' }}
+						initialValues={{ text: '' }}
 						onSubmit={(values) => {
-							console.log('test');
 							console.log(values);
+							doRequest(values);
 						}}
 					>
 						{({ setFieldValue }) => (
 							<Form>
 								<SimpleMDE
-									name="body"
-									onChange={(value) => setFieldValue('body', value)}
+									name="text"
+									onChange={(value) => setFieldValue('text', value)}
 								/>
 								<Button
 									variant="contained"
@@ -75,11 +108,19 @@ export const getServerSideProps = async ({ req, query }) => {
 	);
 
 	const mdxContent = await renderToString(post.body);
+	const comments = [];
+
+	for (const comment of post.comments) {
+		comments.push(await renderToString(comment.text));
+	}
+
+	console.log(comments);
 
 	return {
 		props: {
 			post,
 			mdxContent,
+			comments,
 		},
 	};
 };
