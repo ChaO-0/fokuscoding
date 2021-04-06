d476184452c1acb7d7dc2e5176270791088cf92c
Christopher Yu <christophergin88@gmail.com>
2021-04-07 01:14:17 +0800

    added my posts

---
+++ client/pages/post/index.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import moment from 'moment';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { LinearProgress } from '@material-ui/core';
+
+import Layout from '../../components/Layout';
+import PostList from '../../components/PostList';
+
+const MyPost = () => {
+	const [nextPosts, setNextPosts] = useState([]);
+	const [offset, setOffset] = useState(0);
+	const [hasMore, setHasMore] = useState(true);
+
+	const fetchMoreData = async () => {
+		const username = JSON.parse(localStorage.getItem('currentUser')).username;
+		const { data } = await axios.get(
+			`/api/posts?offset=${offset}&limit=10&username=${username}`
+		);
+		if (data.docs.length === 0) {
+			setHasMore(false);
+		}
+
+		setTimeout(() => {
+			setNextPosts((prev) => [...prev, ...data.docs]);
+		}, 1500);
+		setOffset((prev) => prev + 10);
+	};
+
+	useEffect(async () => {
+		const username = JSON.parse(localStorage.getItem('currentUser')).username;
+		const { data } = await axios.get(
+			`/api/posts?offset=${offset}&limit=10&username=${username}`
+		);
+
+		if (data.docs.length === 0) {
+			setHasMore(false);
+		}
+
+		setNextPosts(data.docs);
+		setOffset((prev) => prev + 10);
+	}, []);
+
+	return (
+		<Layout>
+			<>
+				<InfiniteScroll
+					dataLength={nextPosts.length}
+					next={fetchMoreData}
+					hasMore={hasMore}
+					loader={<LinearProgress />}
+				>
+					{nextPosts.map((post) => (
+						<PostList
+							key={post.id}
+							title={post.title}
+							voteCount={
+								post.votes.filter((vote) => vote.type === 'up').length -
+								post.votes.filter((vote) => vote.type === 'down').length
+							}
+							tags={post.tags}
+							createdBy={post.username}
+							time={moment(post.updatedAt).fromNow()}
+							postId={post.id}
+						/>
+					))}
+				</InfiniteScroll>
+			</>
+		</Layout>
+	);
+};
+
+export default MyPost;
