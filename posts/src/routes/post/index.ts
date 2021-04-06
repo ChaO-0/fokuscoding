67a1662eef212e8a6d31030bdf33a2010a5ce611
Christopher Yu <christophergin88@gmail.com>
2021-04-07 01:14:06 +0800

    updated index post route back end

--- posts/src/routes/post/index.ts
+++ posts/src/routes/post/index.ts
@@ -6,17 +6,36 @@ const router = express.Router();
 router.get('/api/posts', async (req: Request, res: Response) => {
 	const limit: number = parseInt(req.query.limit as string);
 	const offset: number = parseInt(req.query.offset as string);
-	const posts = await Post.paginate(
-		{},
-		{
-			populate: ['votes', 'comments', 'tags'],
-			offset: offset || 0,
-			limit: limit || 10,
-			sort: {
-				updatedAt: 'desc',
+	const username: string = req.query.username as string;
+
+	let posts;
+	if (username) {
+		posts = await Post.paginate(
+			{
+				username,
 			},
-		}
-	);
+			{
+				populate: ['votes', 'comments', 'tags'],
+				offset: offset || 0,
+				limit: limit || 10,
+				sort: {
+					updatedAt: 'desc',
+				},
+			}
+		);
+	} else {
+		posts = await Post.paginate(
+			{},
+			{
+				populate: ['votes', 'comments', 'tags'],
+				offset: offset || 0,
+				limit: limit || 10,
+				sort: {
+					updatedAt: 'desc',
+				},
+			}
+		);
+	}
 
 	res.send(posts);
 });
