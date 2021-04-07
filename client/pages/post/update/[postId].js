d9a06751a12a9d4aa42ef97fede0b4c64ea77cc1
Christopher Yu <christophergin88@gmail.com>
2021-04-08 02:55:40 +0800

    added new post update shit

---
+++ client/pages/post/update/[postId].js
@@ -0,0 +1,47 @@
+import Layout from '../../../components/Layout';
+import PostForm from '../../../components/PostForm';
+import axios from 'axios';
+
+const UpdatePost = ({ tags, post }) => {
+	return (
+		<Layout>
+			<>
+				<h1>Test</h1>
+				<PostForm tags={tags} postValue={post} />
+			</>
+		</Layout>
+	);
+};
+
+export const getServerSideProps = async ({ req, query }) => {
+	const { data } = await axios.get(`${process.env.INGRESS_URI}/api/tags`, {
+		headers: req.headers,
+	});
+
+	data.forEach((elm) => {
+		delete elm.description;
+		delete elm.status;
+		delete elm.version;
+		delete elm.username;
+	});
+
+	const { postId } = query;
+
+	const { data: post } = await axios.get(
+		`${process.env.INGRESS_URI}/api/posts/${postId}`,
+		{
+			headers: req.headers,
+		}
+	);
+
+	console.log(post);
+
+	return {
+		props: {
+			tags: data,
+			post,
+		},
+	};
+};
+
+export default UpdatePost;
