87585a13386169c64d903a805546ea296065631f
Fajar Alnito <fajaralnito18@gmail.com>
2021-04-09 17:13:35 +0800

    add dialogbox component

---
+++ client/components/MyDialogBox.js
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
+
+import {
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+} from '@material-ui/core';
+
+const SimpleMDE = dynamic(() => import('./SimpleMDE'), {
+	ssr: false,
+});
+
+const MyDialogBox = ({
+	buttonText,
+	buttonColor,
+	dialogTitle,
+	dialogText,
+	acceptText,
+	request,
+	showForm,
+}) => {
+	const [open, setOpen] = useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+	const handleRequest = async () => {
+		await request();
+		setOpen(false);
+	};
+	return (
+		<>
+			<Button onClick={handleClickOpen} style={{ color: buttonColor }}>
+				{buttonText}
+			</Button>
+			<Dialog
+				open={open}
+				onClose={handleClose}
+				fullWidth={showForm}
+				maxWidth={showForm && 'md'}
+				aria-labelledby="alert-dialog-title"
+				aria-describedby="alert-dialog-description"
+			>
+				<DialogTitle id="alert-dialog-title">{dialogTitle}</DialogTitle>
+				<DialogContent>
+					<DialogContentText id="alert-dialog-description">
+						{dialogText}
+					</DialogContentText>
+					{showForm && <SimpleMDE value={showForm} />}
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose} color="primary">
+						Cancel
+					</Button>
+					<Button
+						onClick={handleRequest}
+						style={{ color: buttonColor }}
+						autoFocus
+					>
+						{acceptText}
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</>
+	);
+};
+
+export default MyDialogBox;
