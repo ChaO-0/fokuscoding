8ceb2ebaf9dcb81b8997d719b6a459bccf75d5d8
Fajar Alnito <fajaralnito18@gmail.com>
2021-04-19 13:12:49 +0800

    default linewrapping

--- client/components/SimpleMDE.js
+++ client/components/SimpleMDE.js
@@ -4,15 +4,13 @@ import 'easymde/dist/easymde.min.css';
 const DynamicSimpleMDE = (props) => {
 	return (
 		<SimpleMDE
-			style={{
-				maxWidth: '1080px',
-			}}
+			id="MDEEEEEEE"
+			style={{}}
 			{...props}
 			options={{
 				hideIcons: ['guide', 'fullscreen', 'side-by-side', 'preview'],
 				showIcons: ['code', 'table'],
 				spellChecker: false,
-				lineWrapping: false,
 			}}
 		/>
 	);
