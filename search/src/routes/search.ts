5aceac51c3e13df8661af5a7fa3dd05c25d87e3f
Christopher Yu <christophergin88@gmail.com>
2021-04-23 02:30:17 +0800

    updated search service

--- search/src/routes/search.ts
+++ search/src/routes/search.ts
@@ -2,20 +2,41 @@ import { validateRequest } from '@heapoverflow/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Post } from '../models/Post';
+import { Tag } from '../models/Tag';
 
 const router = express.Router();
 
-router.post(
-	'/api/search',
-	[
-		body('query')
-			.isLength({ min: 3 })
-			.withMessage('keyword must be 3 characters'),
-	],
-	validateRequest,
-	async (req: Request, res: Response) => {
-		const { query } = req.body;
-		const search = await Post.find({
+router.post('/api/search', async (req: Request, res: Response) => {
+	const { query, tags } = req.body;
+
+	let search;
+
+	// if tags and query post parameter is not empty
+	if (tags && query) {
+		const alikeTags = await Tag.find({
+			name: {
+				$in: tags,
+			},
+		});
+
+		search = await Post.find({
+			title: {
+				$regex: `.*${query}.*`,
+				$options: 'i',
+			},
+			tags: {
+				$in: alikeTags,
+			},
+		})
+			.sort({ updatedAt: -1 })
+			.populate('tags');
+
+		return res.send(search);
+	}
+
+	// if tags parameter is empty and query parameter is not empty
+	if (!tags && query) {
+		search = await Post.find({
 			title: {
 				$regex: `.*${query}.*`,
 				$options: 'i',
@@ -26,6 +47,27 @@ router.post(
 
 		return res.send(search);
 	}
-);
+
+	// if tags parameter is not empty and query post parameter is empty
+	if (tags && !query) {
+		const alikeTags = await Tag.find({
+			name: {
+				$in: tags,
+			},
+		});
+
+		search = await Post.find({
+			tags: {
+				$in: alikeTags,
+			},
+		})
+			.sort({ updatedAt: -1 })
+			.populate('tags');
+
+		return res.send(search);
+	}
+
+	return res.send(search);
+});
 
 export { router as SearchRouter };
