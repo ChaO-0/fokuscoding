499819e5751ddea64ae62dfd91391e972c729f92
Fajar Alnito <fajaralnito18@gmail.com>
2021-04-26 16:37:40 +0800

    add activate deactivate tags

--- tags/src/routes/delete.ts
+++ tags/src/routes/delete.ts
@@ -1,10 +1,11 @@
 import {
+	BadRequestError,
 	NotAuthorizedError,
 	NotFoundError,
 	requireAuth,
 } from '@heapoverflow/common';
 import express, { Request, Response } from 'express';
-import { Tag } from '../models/Tag';
+import { Tag, TagDoc } from '../models/Tag';
 import { TagDeletedPublisher } from '../events/publishers/tag-deleted-publisher';
 import { natsWrapper } from '../nats-wrapper';
 import { TagStatus } from '../types/tag-status';
@@ -15,7 +16,7 @@ router.delete(
 	'/api/tags/:tag_id',
 	requireAuth,
 	async (req: Request, res: Response) => {
-		const tag = await Tag.findById(req.params.tag_id);
+		const tag: TagDoc = await Tag.findById(req.params.tag_id);
 
 		if (!req.currentUser!.admin) {
 			throw new NotAuthorizedError();
@@ -26,22 +27,35 @@ router.delete(
 		}
 
 		const status = tag.status;
+		const is_active = tag.is_active;
 
-		if (status === TagStatus.Accepted || status === TagStatus.Rejected) {
-			tag.remove();
-			await new TagDeletedPublisher(natsWrapper.client).publish({
-				id: tag.id,
+		if (status === TagStatus.Awaiting) {
+			tag.set({
+				status: TagStatus.Rejected,
 			});
+			await tag.save();
+
+			res.status(204).send(tag);
+		}
+		if (status === TagStatus.Rejected) {
+			tag.remove();
 
 			return res.status(204).send(tag);
 		}
 
-		tag.set({
-			status: TagStatus.Rejected,
-		});
-		await tag.save();
-
-		res.status(204).send(tag);
+		if (status === TagStatus.Accepted && is_active === true) {
+			tag.set({
+				is_active: false,
+			});
+			await tag.save();
+			res.status(204).send(tag);
+		} else if (status === TagStatus.Accepted && is_active === false) {
+			tag.set({
+				is_active: true,
+			});
+			await tag.save();
+			res.status(204).send(tag);
+		}
 	}
 );
 
