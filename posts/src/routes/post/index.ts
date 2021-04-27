639e97257dff1a21551cfaa70b692f84d6f34dc5
Christopher Yu <christophergin88@gmail.com>
2021-04-28 03:57:14 +0800

    refactoring this

--- posts/src/routes/post/index.ts
+++ posts/src/routes/post/index.ts
@@ -4,16 +4,26 @@ import { Tag, TagDoc } from '../../models/Tag';
 const router = express.Router();
 
 router.get('/api/posts', async (req: Request, res: Response) => {
-	const limit: number = parseInt(req.query.limit as string);
-	const offset: number = parseInt(req.query.offset as string);
-	const username: string = req.query.username as string;
-	const has_tags: string = req.query.tags as string;
+	const limit = parseInt(req.query.limit as string);
+	const offset = parseInt(req.query.offset as string);
+	const username = req.query.username as string;
+	const has_tags = req.query.tags as string;
 
-	let tags: TagDoc[] = await Tag.find({
+	let tags = await Tag.find({
 		name: has_tags,
 	});
 
-	let posts;
+	let posts = await Post.paginate(
+		{},
+		{
+			populate: ['votes', 'comments', 'tags'],
+			offset: offset || 0,
+			limit: limit || 10,
+			sort: {
+				updatedAt: 'desc',
+			},
+		}
+	);
 	if (username) {
 		posts = await Post.paginate(
 			{
@@ -28,39 +38,14 @@ router.get('/api/posts', async (req: Request, res: Response) => {
 				},
 			}
 		);
-	} else if (has_tags) {
-		if (offset || limit) {
-			posts = await Post.paginate(
-				{
-					tags: {
-						$in: tags,
-					},
-				},
-				{
-					populate: ['votes', 'comments', 'tags'],
-					offset: offset || 0,
-					limit: limit || 10,
-					sort: {
-						updatedAt: 'desc',
-					},
-				}
-			);
-		} else {
-			posts = await Post.find({
+	}
+	if (has_tags) {
+		posts = await Post.paginate(
+			{
 				tags: {
 					$in: tags,
 				},
-			})
-				.populate('tags')
-				.sort({ updatedAt: 'desc' });
-
-			posts = { postLength: posts.length };
-
-			console.log(posts);
-		}
-	} else {
-		posts = await Post.paginate(
-			{},
+			},
 			{
 				populate: ['votes', 'comments', 'tags'],
 				offset: offset || 0,
@@ -71,6 +56,49 @@ router.get('/api/posts', async (req: Request, res: Response) => {
 			}
 		);
 	}
+	// else if (has_tags) {
+	// 	if (offset || limit) {
+	// 		posts = await Post.paginate(
+	// 			{
+	// 				tags: {
+	// 					$in: tags,
+	// 				},
+	// 			},
+	// 			{
+	// 				populate: ['votes', 'comments', 'tags'],
+	// 				offset: offset || 0,
+	// 				limit: limit || 10,
+	// 				sort: {
+	// 					updatedAt: 'desc',
+	// 				},
+	// 			}
+	// 		);
+	// 	} else {
+	// 		posts = await Post.find({
+	// 			tags: {
+	// 				$in: tags,
+	// 			},
+	// 		})
+	// 			.populate('tags')
+	// 			.sort({ updatedAt: 'desc' });
+
+	// 		posts = { postLength: posts.length };
+
+	// 		console.log(posts);
+	// 	}
+	// } else {
+	// 	posts = await Post.paginate(
+	// 		{},
+	// 		{
+	// 			populate: ['votes', 'comments', 'tags'],
+	// 			offset: offset || 0,
+	// 			limit: limit || 10,
+	// 			sort: {
+	// 				updatedAt: 'desc',
+	// 			},
+	// 		}
+	// 	);
+	// }
 
 	res.status(200).send(posts);
 });
