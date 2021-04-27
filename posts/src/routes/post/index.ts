de5e93634dd91c6453d51282e6d4ad864abf0f36
Fajar Alnito <fajaralnito18@gmail.com>
2021-04-27 17:34:18 +0800

    and find by tags

--- posts/src/routes/post/index.ts
+++ posts/src/routes/post/index.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from 'express';
 import { Post } from '../../models/Post';
-
+import { Tag, TagDoc } from '../../models/Tag';
 const router = express.Router();
 
 router.get('/api/posts', async (req: Request, res: Response) => {
 	const limit: number = parseInt(req.query.limit as string);
 	const offset: number = parseInt(req.query.offset as string);
 	const username: string = req.query.username as string;
+	const has_tags: string = req.query.tags as string;
+
+	let tags: TagDoc[] = await Tag.find({
+		name: has_tags,
+	});
 
 	let posts;
 	if (username) {
@@ -23,6 +28,36 @@ router.get('/api/posts', async (req: Request, res: Response) => {
 				},
 			}
 		);
+	} else if (has_tags) {
+		if (offset || limit) {
+			posts = await Post.paginate(
+				{
+					tags: {
+						$in: tags,
+					},
+				},
+				{
+					populate: ['votes', 'comments', 'tags'],
+					offset: offset || 0,
+					limit: limit || 10,
+					sort: {
+						updatedAt: 'desc',
+					},
+				}
+			);
+		} else {
+			posts = await Post.find({
+				tags: {
+					$in: tags,
+				},
+			})
+				.populate('tags')
+				.sort({ updatedAt: 'desc' });
+
+			posts = { postLength: posts.length };
+
+			console.log(posts);
+		}
 	} else {
 		posts = await Post.paginate(
 			{},
@@ -37,7 +72,7 @@ router.get('/api/posts', async (req: Request, res: Response) => {
 		);
 	}
 
-	res.send(posts);
+	res.status(200).send(posts);
 });
 
 export { router as indexPostRouter };
