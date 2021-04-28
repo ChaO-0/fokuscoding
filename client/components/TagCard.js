7f11f1244c671838f98cdf7e67c60ebf7749c3a3
Christopher Yu <christophergin88@gmail.com>
2021-04-29 01:47:47 +0800

    added link to tagcard

--- client/components/TagCard.js
+++ client/components/TagCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ShowMoreText from 'react-show-more-text';
 import { Card, CardContent, Grid, Typography } from '@material-ui/core';
+import NextLink from 'next/link';
+
 const TagCard = (props) => {
 	const { tagName, tagDesc } = props;
 	return (
@@ -8,9 +10,11 @@ const TagCard = (props) => {
 			<Card>
 				<CardContent>
 					{props.children}
-					<Typography variant="h6" gutterBottom>
-						{tagName}
-					</Typography>
+					<NextLink href={`/tags/${tagName}`}>
+						<Typography variant="h6" gutterBottom style={{ cursor: 'pointer' }}>
+							{tagName}
+						</Typography>
+					</NextLink>
 					<ShowMoreText lines={3}>
 						<Typography variant="body2" align="justify">
 							{tagDesc}
