dbe0b69c627a2ca176f1bdf891a9925b0bb229e7
Christopher Yu <christophergin88@gmail.com>
2024-03-20 20:20:41 +0800

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,29 +1,30 @@
 // Re-export stuff from errors and middlewares
-export * from './errors/bad-request-error';
-export * from './errors/custom-error';
-export * from './errors/database-connection-error';
-export * from './errors/not-authorized-error';
-export * from './errors/not-found-error';
-export * from './errors/request-validation-error';
+//test
+export * from "./errors/bad-request-error";
+export * from "./errors/custom-error";
+export * from "./errors/database-connection-error";
+export * from "./errors/not-authorized-error";
+export * from "./errors/not-found-error";
+export * from "./errors/request-validation-error";
 
-export * from './middlewares/current-user';
-export * from './middlewares/error-handler';
-export * from './middlewares/require-auth';
-export * from './middlewares/validate-request';
+export * from "./middlewares/current-user";
+export * from "./middlewares/error-handler";
+export * from "./middlewares/require-auth";
+export * from "./middlewares/validate-request";
 
-export * from './events/base-listener';
-export * from './events/base-publisher';
-export * from './events/subjects';
+export * from "./events/base-listener";
+export * from "./events/base-publisher";
+export * from "./events/subjects";
 
-export * from './events/tags/tag-created-event';
-export * from './events/tags/tag-deleted-event';
-export * from './events/tags/tag-updated-event';
+export * from "./events/tags/tag-created-event";
+export * from "./events/tags/tag-deleted-event";
+export * from "./events/tags/tag-updated-event";
 
-export * from './events/post/post-created-event';
-export * from './events/post/post-updated-event';
-export * from './events/post/post-deleted-event';
+export * from "./events/post/post-created-event";
+export * from "./events/post/post-updated-event";
+export * from "./events/post/post-deleted-event";
 
-export * from './events/post/vote-count-updated-event';
+export * from "./events/post/vote-count-updated-event";
 
-export * from './events/post/comment-count-updated-event';
-export * from './events/post/solution-updated-event';
+export * from "./events/post/comment-count-updated-event";
+export * from "./events/post/solution-updated-event";
